54588c4027dfca63085ea8e68abda0256df36e9a
Snehashete16 <117341546+Snehashete16@users.noreply.github.com>
2022-11-03 13:36:25 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package basicprogram;
+
+public class HelloWorld {
+public static void main(String []args)
+{
+ int number=32768;
+ System.out.println("the value is:"+number);
+}
+}
---
+++ LoopDemo.java
@@ -0,0 +1,13 @@
+package basicprogram;
+
+public class LoopDemo {
+      public static void main(String[]args)
+      {
+    	  int i;
+    	  for(i=1;i<=50;i++)
+    	  {
+    		  if(i%2==1)
+    		  System.out.println(i);
+    	  }
+     }
+}
